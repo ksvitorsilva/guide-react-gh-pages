53018b842ea897e60867f3a9eb32ab2215550604
ksvitorsilva <ksvitorsilva@users.noreply.github.com>
2020-09-11 16:11:39 +0000

    Deploying to gh-pages from  @ 04b35d9e2e3da5dc44581ec216dc2ea6fc62f17f 🚀

---
+++ precache-manifest.b8b98120fe074e330c7e236c4bb70e12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d6ccf5b3ab572f1f6949c822678e5e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6181f7c5bb1ce65e95a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "af8f8cfc7f160a66238c",
+    "url": "/static/js/2.e1c5d9f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e1c5d9f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6181f7c5bb1ce65e95a",
+    "url": "/static/js/main.98b4c8b4.chunk.js"
+  },
+  {
+    "revision": "abefdd35ed7f744eb50a",
+    "url": "/static/js/runtime-main.c7e52ef1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
